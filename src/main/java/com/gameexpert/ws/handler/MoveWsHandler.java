fbde2784a29634d12646bee517a873045a76e87e
LEE-Hun97 <leehun1997@naver.com>
2026-09-22 10:53:02 +0900

    12. 원래 여러개의 페이지를 열어서 이동을 하는 경우 서로의 화면에서 이동한게 적용이 안되었지만 이제는 실시간으로 이동은 구현되어있다.(설명처럼 웅크리는 경우는 적용이 안된다.)

--- src/main/java/com/gameexpert/ws/handler/MoveWsHandler.java
+++ src/main/java/com/gameexpert/ws/handler/MoveWsHandler.java
@@ -2,6 +2,7 @@ package com.gameexpert.ws.handler;
 
 import com.gameexpert.engine.PlayerAction;
 import com.gameexpert.engine.WorldEngineManager;
+import com.gameexpert.player.entity.Player;
 import com.gameexpert.ws.WsMessageContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,27 @@ public class MoveWsHandler implements WsMessageHandler {
     @Override
     public void handle(WsMessageContext context, JsonNode message) {
         String finalSceneActionId = WsFields.optionalFinalSceneActionId(message);
+        double finalSceneActionX = WsFields.finiteNumber(message, "x");
+        double finalSceneActionY = WsFields.finiteNumber(message, "y");
+        double finalSceneActionZ = WsFields.finiteNumber(message, "z");
+        float finalSceneActionYaw = WsFields.finiteFloat(message, "yaw");
+        float finalSceneActionPitch = WsFields.finiteFloat(message, "pitch");
+        boolean finalSceneActionCrouching = WsFields.booleanValue(message, "crouching");
+        boolean finalSceneActionGliding = WsFields.booleanValue(message, "gliding");
         // TODO Lv 12: 명세의 이동 값을 읽어 현재 사용자의 이동 요청을 엔진에 전달합니다.
+
+        PlayerAction.Move playerActionMove = new PlayerAction.Move(
+                context.nickname(),
+                finalSceneActionX,
+                finalSceneActionY,
+                finalSceneActionZ,
+                finalSceneActionYaw,
+                finalSceneActionPitch,
+                finalSceneActionCrouching,
+                finalSceneActionGliding,
+                finalSceneActionId
+        );
+
+        engineManager.enqueue(context.worldId(), playerActionMove);
     }
 }
